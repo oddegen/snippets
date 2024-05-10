64591509335a85ce441112315483f9fe3c1adca0
oddegen <nahomtensae@gmail.com>
2024-05-10 18:41:20 +0300

    style: rename nav.tsx to nav.ts

---
+++ src/config/nav.ts
@@ -0,0 +1,21 @@
+import { NavConfig } from "@/types";
+
+export const navConfig: NavConfig  = {
+    navItems: [
+        {
+            title: "All Snippets",
+            icon: "list",
+            href: "/"
+        },
+        {
+            title: "Favorites",
+            icon: "star",
+            href: "" //to be changed,
+        },
+        {
+            title: "Untagged",
+            icon: "tag",
+            href: ""
+        }
+    ]
+}
